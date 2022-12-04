f65eb63624173df6def76f010b076f82338da3d9
smaurel <simon.mau85@gmail.com>
2022-12-04 14:31:16 +0100

    Day 2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -19,6 +19,21 @@ impl From<char> for Move {
 }
 
 impl Move {
+    pub fn losing_move(&self) -> Move {
+        match self {
+            Move::Rock => Move::Scissors,
+            Move::Paper => Move::Rock,
+            Move::Scissors => Move::Paper,
+        }
+    }
+
+    pub fn winning_move(&self) -> Move {
+        match self {
+            Move::Rock => Move::Paper,
+            Move::Paper => Move::Scissors,
+            Move::Scissors => Move::Rock,
+        }
+    }
     pub fn score(&self) -> u8 {
         match self {
             Move::Rock => 1,
@@ -29,27 +44,74 @@ impl Move {
 }
 
 #[derive(Debug)]
+pub enum Outcome {
+    Loss,
+    Draw,
+    Win,
+}
+
+impl From<char> for Outcome {
+    fn from(c: char) -> Outcome {
+        match c {
+            'X' => Outcome::Loss,
+            'Y' => Outcome::Draw,
+            'Z' => Outcome::Win,
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl Outcome {
+    pub fn score(&self) -> u8 {
+        match self {
+            Outcome::Loss => 0,
+            Outcome::Draw => 3,
+            Outcome::Win => 6,
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct Round {
     p1_move: Move,
     p2_move: Move,
+    desired_outcome: Outcome,
 }
 
 impl Round {
-    fn new(opponent_move: Move, my_move: Move) -> Self {
-        Round {
-            p1_move: opponent_move,
-            p2_move: my_move,
+    fn from_line(line: &str) -> Self {
+        let chars: Vec<char> = line.split(' ').map(|e| e.chars().next().unwrap()).collect();
+        Self {
+            p1_move: Move::from(chars[0]),
+            p2_move: Move::from(chars[1].clone()),
+            desired_outcome: Outcome::from(chars[1]),
         }
     }
-    fn my_score(&self) -> u8 {
-        let win_points = match (self.p1_move, self.p2_move) {
-            (Move::Rock, Move::Scissors) => 0,
-            (Move::Scissors, Move::Rock) => 6,
-            (p1_move, p2_move) if p2_move.score() > p1_move.score() => 6,
-            (p1_move, p2_move) if p2_move.score() == p1_move.score() => 3,
-            _ => 0,
-        };
-        self.p2_move.score() + win_points
+
+    fn outcome_from_plays(&self) -> Outcome {
+        match (self.p1_move, self.p2_move) {
+            (Move::Rock, Move::Scissors) => Outcome::Loss,
+            (Move::Scissors, Move::Rock) => Outcome::Win,
+            (p1_move, p2_move) if p2_move.score() > p1_move.score() => Outcome::Win,
+            (p1_move, p2_move) if p2_move.score() == p1_move.score() => Outcome::Draw,
+            _ => Outcome::Loss,
+        }
+    }
+
+    fn my_score_from_plays(&self) -> u8 {
+        self.outcome_from_plays().score() + self.p2_move.score()
+    }
+
+    fn move_to_do(&self) -> Move {
+        match self.desired_outcome {
+            Outcome::Loss => self.p1_move.losing_move(),
+            Outcome::Win => self.p1_move.winning_move(),
+            Outcome::Draw => self.p1_move.clone(),
+        }
+    }
+
+    fn score_from_desired_outcome(&self) -> u8 {
+        self.desired_outcome.score() + self.move_to_do().score()
     }
 }
 
@@ -58,23 +120,22 @@ pub fn input_generator(input: &str) -> Vec<Round> {
     input
         .trim()
         .lines()
-        .map(|line| line.split(' '))
-        .map(|line| {
-            line.map(|move_str| Move::from(move_str.chars().next().unwrap()))
-                .collect::<Vec<Move>>()
-        })
-        .map(|line| Round::new(line[0], line[1]))
+        .map(|line| Round::from_line(line))
         .collect()
 }
 
 #[aoc(day2, part1)]
 pub fn solve_part1(rounds: &Vec<Round>) -> u32 {
-    rounds.iter().map(|round| u32::from(round.my_score())).sum()
+    rounds
+        .iter()
+        .map(|round| u32::from(round.my_score_from_plays()))
+        .sum()
 }
 
-// #[aoc(day2, part2)]
-// pub fn solve_part2(rations: &Vec<u32>) -> u32 {
-//     let mut sorted_rations = rations.clone();
-//     sorted_rations.sort();
-//     sorted_rations[sorted_rations.len() - 3..].iter().sum()
-// }
+#[aoc(day2, part2)]
+pub fn solve_part2(rounds: &Vec<Round>) -> u32 {
+    rounds
+        .iter()
+        .map(|round| u32::from(round.score_from_desired_outcome()))
+        .sum()
+}
