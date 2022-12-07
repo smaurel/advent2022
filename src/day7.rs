ef89c8ef58cb493a77d4fd3b672711af6c7c522e
smaurel <simon.mau85@gmail.com>
2022-12-07 23:45:38 +0100

    Day 7

--- src/day7.rs
+++ src/day7.rs
@@ -1,79 +1,145 @@
-use std::{collections::HashSet, hash::Hash};
+use std::{collections::HashSet, hash::Hash, rc::Rc};
 
 use aoc_runner_derive::aoc;
 
+#[derive(PartialEq, Eq, Hash)]
 pub struct Folder {
     size: usize,
-    name: String,
-    path: Path,
+    path: Rc<Path>,
 }
 
+#[derive(PartialEq, Eq, Hash)]
 pub struct Path {
-    path: String,
+    name: String,
     // Represents a stack of directory name, up to /
-    previous_directories: Vec<String>,
+    previous_directory: Option<Rc<Path>>,
 }
 
-pub fn cd_dir(folder_stack: &mut Vec<Folder>, all_folders: &mut HashSet<&Folder>, line: &str) {
-    let dir_name = line.split(' ').last().unwrap();
-    match dir_name {
-        ".." => println!("pop_stack"),
-        _ => push_on_stack(folder_stack, all_folders, line),
+pub struct BrowsingState {
+    folder_stack: Vec<Folder>,
+    other_folders: HashSet<Folder>,
+}
+
+impl BrowsingState {
+    pub fn cd_dir(&mut self, line: &str) {
+        let dir_name = line.split(' ').last().expect("dir");
+        match dir_name {
+            ".." => self.pop_folder(),
+            _ => self.push_on_stack(dir_name),
+        }
     }
-}
 
-pub fn push_on_stack(
-    folder_stack: &mut Vec<Folder>,
-    all_folders: &mut HashSet<&Folder>,
-    dir_name: &str,
-) {
-    let previous_path = folder_stack.last();
-    let new_path = match previous_path {
-        Some(folder) => Path {
-            path: String::from(folder.path.path + "/" + dir_name),
-            previous_directories: folder
-                .path
-                .previous_directories
-                .dedup(String::from(dir_name)),
-        },
-        None => Path {
-            path: String::from(dir_name),
-            previous_directories: vec![],
-        },
-    };
-    let new_folder = Folder {
-        size: 0,
-        name: String::from(dir_name),
-        path: previous_path,
-    };
+    pub fn pop_folder(&mut self) {
+        let popped_dir = self.folder_stack.pop().expect("pop");
+        let new_current_folder = self.folder_stack.last_mut();
+        match new_current_folder {
+            Some(f) => f.size += popped_dir.size,
+            None => {}
+        };
+        self.other_folders.insert(popped_dir);
+    }
+
+    pub fn push_on_stack(&mut self, dir_name: &str) {
+        let new_path = match self.folder_stack.last() {
+            Some(folder) => Path {
+                name: folder.path.name.to_string() + "/" + dir_name,
+                previous_directory: Some(Rc::clone(&folder.path)),
+            },
+            None => Path {
+                name: dir_name.to_owned(),
+                previous_directory: None,
+            },
+        };
+        let new_folder = Folder {
+            size: 0,
+            path: Rc::new(new_path),
+        };
+        self.folder_stack.push(new_folder);
+    }
+
+    pub fn add_size(&mut self, line: &str) {
+        let size: usize = line
+            .split(' ')
+            .next()
+            .expect("line empty")
+            .parse()
+            .expect(line);
+        let curr_folder = self.folder_stack.last_mut().expect("curr_folder");
+        curr_folder.size += size;
+    }
+
+    pub fn back_to_dir(&mut self) {
+        while self.folder_stack.len() > 0 {
+            self.pop_folder()
+        }
+    }
+
+    pub fn get_under_size(&self, size: usize) -> usize {
+        self.other_folders
+            .iter()
+            .filter(|f| f.size <= size)
+            .map(|f| f.size)
+            .sum()
+    }
+
+    pub fn get_total_size(&self) -> usize {
+        self.other_folders
+            .iter()
+            .find(|f| f.path.name == "/")
+            .expect("could not find root dir")
+            .size
+    }
+
+    pub fn find_smallest(&self, size_to_free: usize) -> &Folder {
+        self.other_folders
+            .iter()
+            .filter(|f| f.size >= size_to_free)
+            .min_by_key(|f| f.size)
+            .expect("could not find min")
+    }
 }
 
 #[aoc(day7, part1)]
 pub fn solve_part1(input: &str) -> usize {
-    let mut folder_stack: Vec<Folder> = vec![];
-    let mut all_folders: HashSet<&Folder> = HashSet::new();
+    let folder_stack: Vec<Folder> = vec![];
+    let other_folders: HashSet<Folder> = HashSet::new();
+    let mut browsing_state = BrowsingState {
+        folder_stack,
+        other_folders,
+    };
     for line in input.lines() {
-        match &line[..5] {
+        match &line[..4] {
             // "push or pop on stack"
-            "$ cd" => push_on_stack(&mut folder_stack, &mut all_folders, line),
-            "$ ls" => println!("doing ls, do nothing"),
-            "dir " => println!("directory in ls, do nothing"),
-            _ => println!("add file size to current dire"),
-        }
+            "$ cd" => browsing_state.cd_dir(line),
+            "$ ls" => {}
+            "dir " => {}
+            _ => browsing_state.add_size(line),
+        };
     }
-    5
-    //     input
-    //         .lines()
-    //         .map(|line| get_first_marker(line, 4))
-    //         .next()
-    //         .unwrap()
+
+    browsing_state.back_to_dir();
+    browsing_state.get_under_size(100000)
 }
 
-// #[aoc(day6, part2)]
-// pub fn solve_part2(input: &str) -> usize {
-//     input
-//         .lines()
-//         .map(|line| get_first_marker(line, 14))
-//         .next()
-//         .unwrap()
-// }
+#[aoc(day7, part2)]
+pub fn solve_part2(input: &str) -> usize {
+    let folder_stack: Vec<Folder> = vec![];
+    let other_folders: HashSet<Folder> = HashSet::new();
+    let mut browsing_state = BrowsingState {
+        folder_stack,
+        other_folders,
+    };
+    for line in input.lines() {
+        match &line[..4] {
+            // "push or pop on stack"
+            "$ cd" => browsing_state.cd_dir(line),
+            "$ ls" => {}
+            "dir " => {}
+            _ => browsing_state.add_size(line),
+        };
+    }
+
+    browsing_state.back_to_dir();
+    let size_to_free = browsing_state.get_total_size() - 40000000;
+    browsing_state.find_smallest(size_to_free).size
+}
