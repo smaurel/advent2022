8e8f5f91614a7b366e8442ec05c4d3b3e845919f
smaurel <smaurel@onestock-retail.com>
2022-12-08 14:12:29 +0100

    Day 8 WIP

--- src/day8.rs
+++ src/day8.rs
@@ -27,9 +27,77 @@ pub fn parse(input: &str) -> (HashMap<(usize, usize), u32>, (usize, usize)) {
     (grid, sizes)
 }
 
+pub fn browse_column(
+    line_index: usize,
+    column_size: &usize,
+    grid: &HashMap<(usize, usize), u32>,
+    reverse: bool,
+    mut visible_trees: HashSet<(usize, usize)>,
+) -> HashSet<(usize, usize)> {
+    let mut highest_for_col = -1;
+    let range = if reverse {
+        (*column_size - 1)..0
+    } else {
+        0..*column_size
+    };
+    for col_index in range {
+        let curr_height = grid
+            .get(&(col_index, line_index))
+            .expect("tried to get outside of grid");
+        if *curr_height as i32 > highest_for_col {
+            highest_for_col = *curr_height as i32;
+            visible_trees.insert((col_index, line_index));
+        }
+        if curr_height == &9 {
+            break;
+        }
+    }
+    visible_trees
+}
+
+pub fn browse_line(
+    col_index: usize,
+    line_size: &usize,
+    grid: &HashMap<(usize, usize), u32>,
+    reverse: bool,
+    mut visible_trees: HashSet<(usize, usize)>,
+) -> HashSet<(usize, usize)> {
+    let mut highest_for_line = -1;
+    let range = if reverse {
+        (*line_size - 1)..0
+    } else {
+        0..*line_size
+    };
+    for line_index in range {
+        let curr_height = grid
+            .get(&(col_index, line_index))
+            .expect("tried to get outside of grid");
+        if *curr_height as i32 > highest_for_line {
+            highest_for_line = *curr_height as i32;
+            visible_trees.insert((col_index, line_index));
+        }
+        if curr_height == &9 {
+            break;
+        }
+    }
+    visible_trees
+}
+
 #[aoc(day8, part1)]
 pub fn solve_part1(
     (grid, (line_size, col_size)): &(HashMap<(usize, usize), u32>, (usize, usize)),
 ) -> usize {
-    5
+    let mut visible_trees: HashSet<(usize, usize)> = HashSet::new();
+    // Browse by column
+    for i in 0..*line_size {
+        visible_trees = browse_column(i, &col_size, &grid, false, visible_trees);
+        visible_trees = browse_column(i, &col_size, &grid, true, visible_trees);
+    }
+
+    for i in 0..*col_size {
+        visible_trees = browse_line(i, &line_size, &grid, false, visible_trees);
+        visible_trees = browse_line(i, &line_size, &grid, true, visible_trees);
+    }
+
+    visible_trees.iter().count()
 }
